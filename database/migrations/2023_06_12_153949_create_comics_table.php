2a3b613f32db7dc1d9e9bf365299ec9443efbbdb
gabrielepetralia <gabri.petralia03@gmail.com>
2023-06-12 18:25:51 +0200

    [add] comics migration and seeder

--- database/migrations/2023_06_12_153949_create_comics_table.php
+++ database/migrations/2023_06_12_153949_create_comics_table.php
@@ -15,6 +15,17 @@ return new class extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
+
+            $table->string('title', 100);
+            $table->text('description')->nullable();
+            $table->text('thumb')->nullable();
+            $table->string('price', 10);
+            $table->string('series', 100);
+            $table->date('sale_date');
+            $table->string('type', 20);
+            $table->text('artists');
+            $table->text('writers');
+
             $table->timestamps();
         });
     }
---
+++ database/seeders/ComicsTableSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Comic;
+
+class ComicsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+      $comics = config('comics');
+      foreach ($comics as $comic) {
+        $new_comic = new Comic();
+
+        $new_comic->title = $comic['title'];
+        $new_comic->description = $comic['description'];
+        $new_comic->thumb = $comic['thumb'];
+        $new_comic->price = $comic['price'];
+        $new_comic->series = $comic['series'];
+        $new_comic->sale_date = $comic['sale_date'];
+        $new_comic->type = $comic['type'];
+        $new_comic->artists = implode(", ", $comic['artists']);
+        $new_comic->writers = implode(", ", $comic['writers']);
+
+        $new_comic->save();
+      }
+    }
+}
